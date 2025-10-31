5baf897eea8ce59a24f59a6ab6c20136a0dbcc9c
Buchala Srivani <buchalasrivani0111@gmail.com>
2025-10-31 21:37:25 +0530

    Create app.js

---
+++ backend/scdb/backend/backend/server/app.js
@@ -0,0 +1,10 @@
+import React from "react";
+import HomePage from "./pages/HomePage";
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 text-gray-800">
+      <HomePage />
+    </div>
+  );
+}
+export default App;
